c61dfdc3a734aa6496f6efccadfce15a125a3ffe
David Fowler <davidfowl@gmail.com>
2022-12-30 02:00:51 -0800

    Another idea - Remove the storage requirement and instead use a one way hash plus the redirect support to implement stickyness

--- Microsoft.AspNetCore.SignalR.LoadBalancing/SignalRSessionAffinity.cs
+++ Microsoft.AspNetCore.SignalR.LoadBalancing/SignalRSessionAffinity.cs
@@ -1,7 +1,9 @@
-﻿
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Connections;
-using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Primitives;
 using Yarp.ReverseProxy.Configuration;
 using Yarp.ReverseProxy.Model;
 using Yarp.ReverseProxy.SessionAffinity;
@@ -14,41 +16,47 @@ public static class SignalRSessionAffinity
     public static IReverseProxyBuilder AddSignalRSessionAffinity(this IReverseProxyBuilder builder)
     {
         builder.Services.AddSingleton<ISessionAffinityPolicy, SignalRAffinity>();
-        builder.Services.AddMemoryCache();
+
+        var hashes = new DestinationHashes();
+        builder.Services.AddSingleton(hashes);
 
         builder.AddTransforms(transforms =>
         {
-            var cache = transforms.Services.GetRequiredService<IMemoryCache>();
-
-            // REVIEW: Is this good enough for SSE and long polling?
-            var cacheEntryOptions = new MemoryCacheEntryOptions()
-            {
-                SlidingExpiration = TimeSpan.FromMinutes(5)
-            };
-
             transforms.AddResponseTransform(async c =>
             {
                 var proxyFeature = c.HttpContext.GetReverseProxyFeature();
 
                 // If this route is marked as a signalr route, then get the negotiate response
-                // and associate the connection id with this destination. The first request can land anywhere
-                // so we need to store the association between the connection id/token and the proxied
-                // destination.
+                // and associate the connection id with this destination.
+
                 if (proxyFeature.Route.Config.Metadata?.ContainsKey("signalr") is true &&
+                    StringValues.IsNullOrEmpty(c.HttpContext.Request.Query["yarp.affinity"]) &&
                     c.ProxyResponse is { IsSuccessStatusCode: true })
                 {
-                    // The response should be small so we can buffer it
-                    var data = await c.ProxyResponse.Content.ReadAsByteArrayAsync(c.HttpContext.RequestAborted);
-                    var negotiateResponse = NegotiateProtocol.ParseResponse(data);
-
-                    // Restore the content so it can be read again
-                    c.ProxyResponse.Content = new ByteArrayContent(data);
-
-                    if (negotiateResponse?.ConnectionToken is string key &&
-                        proxyFeature.ProxiedDestination is { } destination)
+                    // Redirect to the same URL with 
+                    if (proxyFeature.ProxiedDestination is { } destination)
                     {
-                        // Store the association between the destination and the connection token
-                        cache.Set(key, destination.DestinationId, cacheEntryOptions);
+                        var req = c.HttpContext.Request;
+                        var query = req.QueryString.Add("yarp.affinity", hashes.GetDestinationHash(destination));
+
+                        var url = UriHelper.BuildAbsolute(req.Scheme, req.Host, req.PathBase, new(req.Path.Value!.Replace("/negotiate", "")), query);
+
+                        // The negoitate response supports redirecting the client to another URL, we're taking advantage of that here
+                        // to affinitize the request. https://github.com/dotnet/aspnetcore/blob/main/src/SignalR/docs/specs/TransportProtocols.md#all-versions
+                        // back to whatever destination we landed on initially.
+
+                        // This will force a renegotiation.
+
+                        // This can be improved by natively supporting round tripping an affinity token from the response header
+                        // into the query string.
+
+                        c.SuppressResponseBody = true;
+
+                        c.HttpContext.Response.Clear();
+                        await c.HttpContext.Response.WriteAsJsonAsync(new
+                        {
+                            url
+                        });
                     }
                 }
             });
@@ -59,13 +67,9 @@ public static class SignalRSessionAffinity
 
     private sealed class SignalRAffinity : ISessionAffinityPolicy
     {
-        private readonly IMemoryCache _cache;
-
-        public SignalRAffinity(IMemoryCache cache)
-        {
-            _cache = cache;
-        }
-
+        private readonly DestinationHashes _hashes;
+        public SignalRAffinity(DestinationHashes hashes) => _hashes = hashes;
+        
         public string Name => "SignalR";
 
         public void AffinitizeResponse(HttpContext context, ClusterState cluster, SessionAffinityConfig config, DestinationState destination)
@@ -75,24 +79,36 @@ public static class SignalRSessionAffinity
 
         public AffinityResult FindAffinitizedDestinations(HttpContext context, ClusterState cluster, SessionAffinityConfig config, IReadOnlyList<DestinationState> destinations)
         {
-            string? token = context.Request.Query["id"];
-            if (token is not null && _cache.TryGetValue<string>(token, out var destinationId))
+            string? affinity = context.Request.Query["yarp.affinity"];
+            if (affinity is not null)
             {
                 foreach (var d in destinations)
                 {
-                    if (d.DestinationId == destinationId)
+                    var hash = _hashes.GetDestinationHash(d);
+
+                    if (hash == affinity)
                     {
                         return new(d, AffinityStatus.OK);
                     }
                 }
 
-                // Remove the assocition if we can't find the destination
-                _cache.Remove(token);
-
                 return new(null, AffinityStatus.DestinationNotFound);
             }
 
             return new(null, AffinityStatus.AffinityKeyNotSet);
+        }
+    }
+    private class DestinationHashes
+    {
+        private readonly ConditionalWeakTable<DestinationState, string> _hashes = new();
+
+        public string GetDestinationHash(DestinationState destination)
+        {
+            return _hashes.GetValue(destination, static d =>
+            {
+                var destinationUtf8Bytes = Encoding.UTF8.GetBytes(d.DestinationId.ToUpperInvariant());
+                return Convert.ToHexString(SHA256.HashData(destinationUtf8Bytes)).ToLowerInvariant();
+            });
         }
     }
 }
