d29f65b9fc81f6022782745d69ad39ec8c3d8f62
David Fowler <davidfowl@gmail.com>
2023-02-11 10:38:42 -0800

    Remove the original prototype and pivot to smart session affinity

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -11,32 +11,9 @@ if (redisConnection is not null)
     signalr.AddStackExchangeRedis(redisConnection);
 }
 
-//builder.Services.AddSmartLoadBalancing(options =>
-//{
-//    // Configure the ingress url. If it's null, the middleware will use
-//    // "X-Forwarded-Host" to determine the URL of the load balancer.
-//    options.IngressUrl = builder.Configuration.GetServiceUri("ingress")?.ToString();
-//});
-
 var app = builder.Build();
 
-// This id helps identify server instances
-var id = Guid.NewGuid().ToString();
-
-app.Use((context, next) =>
-{
-    context.Response.OnStarting(() =>
-    {
-        context.Response.Headers["X-Server"] = id;
-        return Task.CompletedTask;
-    });
-
-    return next(context);
-});
-
 app.UseFileServer();
-
-// app.UseSmartLoadBalancing();
 
 app.MapHub<Chat>("/chat");
 
